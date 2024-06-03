86d370ad6c56927c82aaa4a3f18ec51851c355e1
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-03 16:40:01 -0500

    fixed all css

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -13,24 +13,28 @@ export default function Resume() {
     return (
         <div>
 
-            <a href="/resume.pdf" download className="download">Click Here To Download Resume</a>
-            <h3>My Proficiencies</h3>
-            <ul className="Proficiencies">
-                <li>Javascript</li>
-                <li>CSS</li>
-                <li>HTML</li>
-                <li>PostgresSQl</li>
-                <li>GitHub</li>
-                <li>MongoDB</li>
-                <li>Express</li>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>Handlebars</li>
-                <li>jQuery</li>
-                <li>Bootstrap</li>
-                <li>Sequelize</li>
-                <li>Mongoose</li>
+            <h3 className="prof-title">My Proficiencies</h3>
+            <ul className="proficiencies">
+                <div className="prof-cont">
+                    <li>Javascript</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>PostgresSQl</li>
+                    <li>GitHub</li>
+                    <li>MongoDB</li>
+                    <li>Express</li>
+                </div>
+                <div className="prof-cont">
+                    <li>React</li>
+                    <li>Node.js</li>
+                    <li>Handlebars</li>
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                    <li>Sequelize</li>
+                    <li>Mongoose</li>
+                </div>
             </ul>
+            <a href="/resume.pdf" download className="download">Click Here To Download Resume</a>
         </div>
     )
 
